c2c02ba3db5d0ec84a7bbaeb45a3c19355e7f4bf
dianuj1997 <dianujkotov15@gmail.com>
2021-06-15 13:41:35 +0900

    fourth commit

--- lib/beacon_csv.dart
+++ lib/beacon_csv.dart
@@ -86,7 +86,7 @@ class IBeaconCard extends StatelessWidget {
 
     /// We require the initializers to run after the loading screen is rendered
     Timer(Duration(seconds: 20), () async{
-      String myuuid_bro=await getmyuuid();
+      String myuuid_bro=await pleasegetmyuuid();
       String myusername_bro=await getmyusername();
       print("Yeah, this line is printed after 3 seconds");
       // var disty=double.parse(b);
@@ -325,5 +325,13 @@ Future<String> getmyusername() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   //Return String
   String stringValue = prefs.getString('myusername');
+  return stringValue;
+}
+
+
+Future<String> pleasegetmyuuid() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  //Return String
+  String stringValue = prefs.getString('pleasemyuuid');
   return stringValue;
 }
